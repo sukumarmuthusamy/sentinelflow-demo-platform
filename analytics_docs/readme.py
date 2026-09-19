553443d050718b0bc334eace4a32c1bf88708a32
Sukumar <sukumarm1994@gmail.com>
2026-09-18 19:47:19 -0700

    docs: expand analytics documentation

--- analytics_docs/readme.py
+++ analytics_docs/readme.py
@@ -5,4 +5,8 @@ README = """
 
 Synthetic documentation for svc-analytics reporting pipelines.
 This module is intentionally non-code and should not affect blast radius.
+
+## Reporting cadence
+- Daily aggregate rollups at 02:00 UTC
+- Weekly payer mix reports on Mondays
 """
